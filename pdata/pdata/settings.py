f96b0f3ac2a25b0f7248774884ba3d7189d2c0b4
Rushy Panchal <panchal.rushy@gmail.com>
2018-01-12 14:50:46 -0500

    Load database configuration from DATABASE_URL

--- pdata/pdata/settings.py
+++ pdata/pdata/settings.py
@@ -8,28 +8,29 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ENV = os.getenv('ENV', 'development')
 SECRET_KEY = os.getenv(
-    'SECRET_KEY', '-@8$+sra*9xl%+ed@h)%3mzs(@vsar_0zg6jq*q!&y^vx3ar3!')
-DEBUG = True
-ALLOWED_HOSTS = []
+  'SECRET_KEY', '-@8$+sra*9xl%+ed@h)%3mzs(@vsar_0zg6jq*q!&y^vx3ar3!')
+DEBUG = (ENV == 'development')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
 
-    # pdata
-    'core',
+  # pdata
+  'core',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'pdata.urls'
@@ -37,28 +38,26 @@ WSGI_APPLICATION = 'pdata.wsgi.application'
 
 # Database
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 # Internationalization
